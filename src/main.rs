a60df4cb60d63095b4e0bf3fbe1b357d72fc3ba9
izonion <isaackeet@gmail.com>
2022-01-22 15:17:57 -0700

    Change 4space to tabs

--- src/main.rs
+++ src/main.rs
@@ -20,261 +20,265 @@ const BOX_SIZE: i16 = 17;
 
 /// Representation of the application state. In this example, a box will bounce around the screen.
 struct World {
-    transforms: Vec<ScreenTransform>,
+	transforms: Vec<ScreenTransform>,
 }
 
 fn main() -> Result<(), Error> {
-    env_logger::init();
-    let event_loop = EventLoop::new();
-    let mut input = WinitInputHelper::new();
-    let window = {
-        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-        WindowBuilder::new()
-            .with_title("Hello Pixels")
-            .with_inner_size(size)
-            .with_min_inner_size(size)
-            .build(&event_loop)
-            .unwrap()
-    };
+	env_logger::init();
+	let event_loop = EventLoop::new();
+	let mut input = WinitInputHelper::new();
+	let window = {
+		let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+		WindowBuilder::new()
+			.with_title("Hello Pixels")
+			.with_inner_size(size)
+			.with_min_inner_size(size)
+			.build(&event_loop)
+			.unwrap()
+	};
 
-    let mut pixels = {
-        let window_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture)?
-    };
-    let mut world = World::new();
+	// let mut pixels = {
+		let window_size = window.inner_size();
+		let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+		Pixels::new(WIDTH, HEIGHT, surface_texture)?
+	};
+	let mut world = World::new();
 
-    let clear_buffer = {
-        let mut clear_buffer = [0u8; (WIDTH * HEIGHT * 4) as usize];
-        for i in 0..(WIDTH * HEIGHT) as usize {
-            clear_buffer[i * 4..i * 4 + 4].copy_from_slice(&[0xE3, 0xE3, 0xE3, 0xff]);
-        }
-        Box::new(clear_buffer) as Box<[u8]>
-    };
+	let clear_buffer = {
+		let mut clear_buffer = [0u8; (WIDTH * HEIGHT * 4) as usize];
+		for i in 0..(WIDTH * HEIGHT) as usize {
+			clear_buffer[i * 4..i * 4 + 4].copy_from_slice(&[0xE3, 0xE3, 0xE3, 0xff]);
+		}
+		Box::new(clear_buffer) as Box<[u8]>
+	};
 
-    let mut last_frame = Instant::now();
-    let mut cumulative_delta = Duration::from_secs_f64(0.0);
-    let mut frame_count = 0;
-    let max_frame_count = 60;
-    event_loop.run(move |event, _, control_flow| {
-        // Draw the current frame
-        if let Event::RedrawRequested(_) = event {
-            world.draw(&clear_buffer, pixels.get_frame());
-            if pixels
-                .render()
-                .map_err(|e| error!("pixels.render() failed: {}", e))
-                .is_err()
-            {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
+	let mut last_frame = Instant::now();
+	let mut cumulative_delta = Duration::from_secs_f64(0.0);
+	let mut frame_count = 0;
+	let max_frame_count = 60;
+	event_loop.run(move |event, _, control_flow| {
+		// Draw the current frame
+		if let Event::RedrawRequested(_) = event {
+			world.draw(&clear_buffer, pixels.get_frame());
+			if pixels
+				.render()
+				.map_err(|e| error!("pixels.render() failed: {}", e))
+				.is_err()
+			{
+				*control_flow = ControlFlow::Exit;
+				return;
+			}
+		}
 
-        // Handle input events
-        if input.update(&event) {
-            let current_frame = Instant::now();
-            let delta_frame = current_frame - last_frame;
-            cumulative_delta += delta_frame;
-            frame_count += 1;
-            if frame_count == max_frame_count {
-                println!("{:?}", cumulative_delta / max_frame_count);
-                frame_count = 0;
-                cumulative_delta = Duration::from_secs_f64(0.0);
-            }
-            last_frame = current_frame;
+		// Handle input events
+		if input.update(&event) {
+			let current_frame = Instant::now();
+			let delta_frame = current_frame - last_frame;
+			cumulative_delta += delta_frame;
+			frame_count += 1;
+			if frame_count == max_frame_count {
+				println!("{:?}", cumulative_delta / max_frame_count);
+				frame_count = 0;
+				cumulative_delta = Duration::from_secs_f64(0.0);
+			}
+			last_frame = current_frame;
 
-            // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
+			// Close events
+			if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+				*control_flow = ControlFlow::Exit;
+				return;
+			}
 
-            // Resize the window
-            if let Some(size) = input.window_resized() {
-                pixels.resize_surface(size.width, size.height);
-            }
+			// Resize the window
+			if let Some(size) = input.window_resized() {
+				pixels.resize_surface(size.width, size.height);
+			}
 
-            let mouse_state = if input.mouse_pressed(0) {
-                MouseClickState::Pressed
-            } else if input.mouse_released(0) {
-                MouseClickState::Released
-            } else if input.mouse_held(0) {
-                MouseClickState::Held
-            } else {
-                MouseClickState::Idle
-            };
+			let mouse_state = if input.mouse_pressed(0) {
+				MouseClickState::Pressed
+			} else if input.mouse_released(0) {
+				MouseClickState::Released
+			} else if input.mouse_held(0) {
+				MouseClickState::Held
+			} else {
+				MouseClickState::Idle
+			};
 
-            let mouse_pos = if let Some(mouse_pos) = input.mouse() {
-                if let Ok((x, y)) = pixels.window_pos_to_pixel(mouse_pos) {
-                    Some((x as f32, y as f32))
-                } else { None }
-            } else { None };
+			let mouse_pos = if let Some(mouse_pos) = input.mouse() {
+				if let Ok((x, y)) = pixels.window_pos_to_pixel(mouse_pos) {
+					Some((x as f32, y as f32))
+				} else { None }
+			} else { None };
 
-            // Update internal state and request a redraw
-            world.update(mouse_pos, mouse_state);
-            window.request_redraw();
-        }
-    });
+			// Update internal state and request a redraw
+			world.update(mouse_pos, mouse_state);
+			window.request_redraw();
+		}
+	});
 }
 
 #[derive(Copy, Clone)]
 enum MouseClickState {
-    Pressed,
-    Held,
-    Released,
-    Idle,
+	Pressed,
+	Held,
+	Released,
+	Idle,
 }
 
 impl World {
-    /// Create a new `World` instance that can draw a moving box.
-    fn new() -> Self {
-        let mut transforms = Vec::new();
-        transforms.push(ScreenTransform { transform: pixel::Transform {
-            position: pixel::Vec2::new(WIDTH as f32 / 2.0, HEIGHT as f32 / 2.0),
-            rotation: 0.0,
-            scale: 0.6,
-            alpha: 0xff,
-        }, hovering: None, grabbing: None});
-        Self {
-            transforms,
-        }
-    }
+	/// Create a new `World` instance that can draw a moving box.
+	fn new() -> Self {
+		let mut transforms = Vec::new();
+		transforms.push(ScreenTransform { transform: pixel::Transform {
+			position: pixel::Vec2::new(WIDTH as f32 / 2.0, HEIGHT as f32 / 2.0),
+			rotation: 0.0,
+			scale: 0.6,
+			alpha: 0xff,
+		}, hovering: None, grabbing: None});
+		Self {
+			transforms,
+		}
+	}
 
-    fn update(&mut self, mouse_pos: Option<(f32, f32)>, mouse_state: MouseClickState) {
-        for transform in &mut self.transforms {
-            if let Some((x, y)) = mouse_pos {
-                transform.mouse_input(pixel::Vec2::new(x, y), mouse_state);
-            }
-        }
-    }
+	fn update(&mut self, mouse_pos: Option<(f32, f32)>, mouse_state: MouseClickState) {
+		for transform in &mut self.transforms {
+			if let Some((x, y)) = mouse_pos {
+				transform.mouse_input(pixel::Vec2::new(x, y), mouse_state);
+			}
+		}
+	}
 
-    /// Draw the `World` state to the frame buffer.
-    ///
-    /// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
-    fn draw(&self, clear_buffer: &Box<[u8]>, frame: &mut [u8]) {
-        // Can extend to not have to make a new vec every time
-        frame.copy_from_slice(clear_buffer);
-        let mut grid = pixel::PixelGrid(frame);
-        for transform in &self.transforms {
-            transform.draw(&mut grid);
-        }
-    }
+	/// Draw the `World` state to the frame buffer.
+	///
+	/// Assumes the default texture format: `wgpu::TextureFormat::Rgba8UnormSrgb`
+	fn draw(&self, clear_buffer: &Box<[u8]>, frame: &mut [u8]) {
+		// Can extend to not have to make a new vec every time
+		frame.copy_from_slice(clear_buffer);
+		let mut grid = pixel::PixelGrid(frame);
+		for transform in &self.transforms {
+			transform.draw(&mut grid);
+		}
+	}
 }
 
 #[derive(Eq, PartialEq, Copy, Clone)]
 enum Hoverables {
-    Rotate,
-    Translate,
-    Scale,
-    Alpha,
+	Rotate,
+	Translate,
+	Scale,
+	Alpha,
 }
 
 struct ScreenTransform {
-    transform: pixel::Transform,
-    hovering: Option<Hoverables>,
-    grabbing: Option<Hoverables>,
+	transform: pixel::Transform,
+	hovering: Option<Hoverables>,
+	grabbing: Option<Hoverables>,
 }
 
 const ROTATE_HOVERABLE: [f32; 4] = [-85.0, -200.0, 85.0, -140.0]; // Two bounding points
+const TRANSLATE_HOVERABLE: [f32; 4] = [-85.0, -200.0, 85.0, -140.0]; // Two bounding points
 
 impl ScreenTransform {
-    fn draw(&self, grid: &mut pixel::PixelGrid) {
-        let width = WIDTH as f32;
-        let height = HEIGHT as f32;
+	fn draw(&self, grid: &mut pixel::PixelGrid) {
+		let width = WIDTH as f32;
+		let height = HEIGHT as f32;
 
-        let unhoverable_color = [0x13, 0x1B, 0x23];
-        let hoverable_color = [0xA6, 0x26, 0x39];
-        let hovering_color = [0xDB, 0x32, 0x4D];
-        let clicking_color = [0x85, 0x1E, 0x2E];
+		let unhoverable_color = [0x13, 0x1B, 0x23];
+		let hoverable_color = [0xA6, 0x26, 0x39];
+		let hovering_color = [0xDB, 0x32, 0x4D];
+		let clicking_color = [0x85, 0x1E, 0x2E];
 
-        // let precision_scale = 1.0;
-        let mut x = -width / 2.0;
-        while x < width / 2.0 {
-            for line_width in 0..10 {
-                grid.set_pixel_transformed( pixel::Vec2::new(x, -height / 2.0 + (line_width as f32) / self.transform.scale),
-                                            &self.transform,
-                                            &unhoverable_color);
-                grid.set_pixel_transformed( pixel::Vec2::new(x, height / 2.0 - (line_width as f32) / self.transform.scale),
-                                            &self.transform,
-                                            &unhoverable_color);
-            }
-            x += self.transform.scale;
-        }
-        let mut y = -height / 2.0;
-        while y < height / 2.0 {
-            for line_width in 0..10 {
-                grid.set_pixel_transformed( pixel::Vec2::new(-width / 2.0 + (line_width as f32) / self.transform.scale, y),
-                                            &self.transform,
-                                            &unhoverable_color);
-                grid.set_pixel_transformed( pixel::Vec2::new(width / 2.0 - (line_width as f32) / self.transform.scale, y),
-                                            &self.transform,
-                                            &unhoverable_color);
-            }
-            y += self.transform.scale;
-        }
+		// let precision_scale = 1.0;
+		let mut x = -width / 2.0;
+		while x < width / 2.0 {
+			for line_width in 0..10 {
+				grid.set_pixel_transformed( pixel::Vec2::new(x, -height / 2.0 + (line_width as f32) / self.transform.scale),
+											&self.transform,
+											&unhoverable_color);
+				grid.set_pixel_transformed( pixel::Vec2::new(x, height / 2.0 - (line_width as f32) / self.transform.scale),
+											&self.transform,
+											&unhoverable_color);
+			}
+			x += self.transform.scale;
+		}
+		let mut y = -height / 2.0;
+		while y < height / 2.0 {
+			for line_width in 0..10 {
+				grid.set_pixel_transformed( pixel::Vec2::new(-width / 2.0 + (line_width as f32) / self.transform.scale, y),
+											&self.transform,
+											&unhoverable_color);
+				grid.set_pixel_transformed( pixel::Vec2::new(width / 2.0 - (line_width as f32) / self.transform.scale, y),
+											&self.transform,
+											&unhoverable_color);
+			}
+			y += self.transform.scale;
+		}
 
-        let rotate_color =
-            if self.grabbing == Some(Hoverables::Rotate) { &clicking_color }
-            else if self.hovering == Some(Hoverables::Rotate) { &hovering_color }
-            else { &hoverable_color };
-        for x in -75..75 {
-            let outer_arc = (100.0*100.0 - x as f32 * x as f32).sqrt() as i16;
-            let inner_arc = (75.0*75.0 - x as f32 * x as f32).sqrt() as i16;
-            for y in inner_arc.max(50)..outer_arc {
-                grid.set_pixel_transformed( pixel::Vec2::new(x as f32, -y as f32 - 100.0),
-                                            &self.transform,
-                                            &rotate_color);
-            }
-        }
-        for x in 40..85 {
-            for y in 40..x {
-                grid.set_pixel_transformed( pixel::Vec2::new(x as f32, -y as f32 - 100.0),
-                                            &self.transform,
-                                            &rotate_color);
-                grid.set_pixel_transformed( pixel::Vec2::new(-x as f32, -y as f32 - 100.0),
-                                            &self.transform,
-                                            &rotate_color);
-            }
-        }
-    }
+		let rotate_color =
+			if self.grabbing == Some(Hoverables::Rotate) { &clicking_color }
+			else if self.hovering == Some(Hoverables::Rotate) { &hovering_color }
+			else { &hoverable_color };
+		for x in -75..75 {
+			let outer_arc = (100.0*100.0 - x as f32 * x as f32).sqrt() as i16;
+			let inner_arc = (75.0*75.0 - x as f32 * x as f32).sqrt() as i16;
+			for y in inner_arc.max(50)..outer_arc {
+				grid.set_pixel_transformed( pixel::Vec2::new(x as f32, -y as f32 - 100.0),
+											&self.transform,
+											&rotate_color);
+			}
+		}
+		for x in 40..85 {
+			for y in 40..x {
+				grid.set_pixel_transformed( pixel::Vec2::new(x as f32, -y as f32 - 100.0),
+											&self.transform,
+											&rotate_color);
+				grid.set_pixel_transformed( pixel::Vec2::new(-x as f32, -y as f32 - 100.0),
+											&self.transform,
+											&rotate_color);
+			}
+		}
+	}
 
-    fn mouse_input(&mut self, pos: pixel::Vec2, mouse_state: MouseClickState) {
-        let local_pos = self.transform.apply_inverse(pos);
-        if let Some(grabbing) = self.grabbing {
-            self.hovering = None;
-            match grabbing {
-                Hoverables::Rotate => {
-                    let pos = pos - self.transform.position;
-                    let angle = if pos.y >= 0.0 {
-                        -(pos.x / pos.y).atan() + std::f32::consts::PI
-                    } else {
-                        -(pos.x / pos.y).atan()
-                    };
-                    self.transform.rotation = angle;
-                },
-                _ => (),
-            }
-            match mouse_state {
-                MouseClickState::Released => {
-                    self.grabbing = None;
-                },
-                _ => (),
-            }
-        } else {
-            if      local_pos.x > ROTATE_HOVERABLE[0] &&
-                    local_pos.x < ROTATE_HOVERABLE[2] &&
-                    local_pos.y > ROTATE_HOVERABLE[1] &&
-                    local_pos.y < ROTATE_HOVERABLE[3] {
-                self.hovering = Some(Hoverables::Rotate);
-                match mouse_state {
-                    MouseClickState::Pressed => {
-                        self.grabbing = Some(Hoverables::Rotate);
-                    },
-                    _ => (),
-                }
-            } else {
-                self.hovering = None;
-            }
-        }
-    }
+	fn mouse_input(&mut self, pos: pixel::Vec2, mouse_state: MouseClickState) {
+		let local_pos = self.transform.apply_inverse(pos);
+		if let Some(grabbing) = self.grabbing {
+			self.hovering = None;
+			match grabbing {
+				Hoverables::Rotate => {
+					let pos = pos - self.transform.position;
+					let angle = if pos.y >= 0.0 {
+						-(pos.x / pos.y).atan() + std::f32::consts::PI
+					} else {
+						-(pos.x / pos.y).atan()
+					};
+					self.transform.rotation = angle;
+				},
+				_ => (),
+			}
+			match mouse_state {
+				MouseClickState::Released => {
+					self.grabbing = None;
+				},
+				_ => (),
+			}
+		} else {
+			if 		local_pos.x > ROTATE_HOVERABLE[0] &&
+					local_pos.x < ROTATE_HOVERABLE[2] &&
+					local_pos.y > ROTATE_HOVERABLE[1] &&
+					local_pos.y < ROTATE_HOVERABLE[3] {
+				self.hovering = Some(Hoverables::Rotate);
+				match mouse_state {
+					MouseClickState::Pressed => {
+						self.grabbing = Some(Hoverables::Rotate);
+					},
+					_ => (),
+				}
+			} else if 	local_pos.x > ROTATE_HOVERABLE[0] &&
+						local_pos.x < ROTATE_HOVERABLE[2] &&
+						local_pos.y > ROTATE_HOVERABLE[1] &&
+						local_pos.y < ROTATE_HOVERABLE[3] else {
+				self.hovering = None;
+			}
+		}
+	}
 }
--- src/pixel.rs
+++ src/pixel.rs
@@ -5,30 +5,30 @@ use crate::{WIDTH, HEIGHT};
 pub struct PixelGrid<'a>(pub &'a mut [u8]);
 
 impl<'a> PixelGrid<'a> {
-    pub fn set_pixel(&mut self, point: Vec2, pixel: &[u8; 3]) {
-        let x = point.x.clamp(0.0, (WIDTH - 1) as f32) as u32;
-        let y = point.y.clamp(0.0, (HEIGHT - 1) as f32) as u32;
-        let i = (x as usize + (y * WIDTH) as usize) * 4;
-        self.0[i..i + 2].copy_from_slice(pixel);
-    }
+	pub fn set_pixel(&mut self, point: Vec2, pixel: &[u8; 3]) {
+		let x = point.x.clamp(0.0, (WIDTH - 1) as f32) as u32;
+		let y = point.y.clamp(0.0, (HEIGHT - 1) as f32) as u32;
+		let i = (x as usize + (y * WIDTH) as usize) * 4;
+		self.0[i..i + 2].copy_from_slice(pixel);
+	}
 
-    pub fn set_pixel_transformed(&mut self, point: Vec2, transform: &Transform, pixel: &[u8; 3]) {
-    	let point = transform.apply(point);
-        let x = point.x.clamp(0.0, (WIDTH - 1) as f32) as u32;
-        let y = point.y.clamp(0.0, (HEIGHT - 1) as f32) as u32;
-        let i = (x as usize + (y * WIDTH) as usize) * 4;
-        let alpha = transform.alpha as f32 / 255.0;
-        self.0[i + 0] = (((self.0[i + 0] as f32 / 255.0) * (1.0 - alpha) + (pixel[0] as f32 / 255.0) * alpha) * 255.0) as u8;
-        self.0[i + 1] = (((self.0[i + 1] as f32 / 255.0) * (1.0 - alpha) + (pixel[1] as f32 / 255.0) * alpha) * 255.0) as u8;
-        self.0[i + 2] = (((self.0[i + 2] as f32 / 255.0) * (1.0 - alpha) + (pixel[2] as f32 / 255.0) * alpha) * 255.0) as u8;
-    }
+	pub fn set_pixel_transformed(&mut self, point: Vec2, transform: &Transform, pixel: &[u8; 3]) {
+		let point = transform.apply(point);
+		let x = point.x.clamp(0.0, (WIDTH - 1) as f32) as u32;
+		let y = point.y.clamp(0.0, (HEIGHT - 1) as f32) as u32;
+		let i = (x as usize + (y * WIDTH) as usize) * 4;
+		let alpha = transform.alpha as f32 / 255.0;
+		self.0[i + 0] = (((self.0[i + 0] as f32 / 255.0) * (1.0 - alpha) + (pixel[0] as f32 / 255.0) * alpha) * 255.0) as u8;
+		self.0[i + 1] = (((self.0[i + 1] as f32 / 255.0) * (1.0 - alpha) + (pixel[1] as f32 / 255.0) * alpha) * 255.0) as u8;
+		self.0[i + 2] = (((self.0[i + 2] as f32 / 255.0) * (1.0 - alpha) + (pixel[2] as f32 / 255.0) * alpha) * 255.0) as u8;
+	}
 }
 
 pub struct Transform {
-    pub position: Vec2,
-    pub rotation: f32,
-    pub scale: f32,
-    pub alpha: u8,
+	pub position: Vec2,
+	pub rotation: f32,
+	pub scale: f32,
+	pub alpha: u8,
 }
 
 impl Transform {
@@ -50,8 +50,8 @@ impl Transform {
 // Vec2
 #[derive(Copy, Clone)]
 pub struct Vec2 {
-    pub x: f32,
-    pub y: f32,
+	pub x: f32,
+	pub y: f32,
 }
 
 impl Vec2 {
@@ -60,8 +60,8 @@ impl Vec2 {
 	}
 
 	fn rotate(&self, angle: f32) -> Self {
-	    let sin = angle.sin();
-	    let cos = angle.cos();
+		let sin = angle.sin();
+		let cos = angle.cos();
 		Self {
 			x: self.x * cos - self.y * sin,
 			y: self.y * cos + self.x * sin,
@@ -78,61 +78,61 @@ impl Vec2 {
 }
 
 impl std::ops::Mul<Self> for Vec2 {
-    type Output = Self;
+	type Output = Self;
 
-    fn mul(self, rhs: Self) -> Self {
-        let x = self.x * rhs.x;
-        let y = self.y * rhs.y;
-        Self::new(x, y)
-    }
+	fn mul(self, rhs: Self) -> Self {
+		let x = self.x * rhs.x;
+		let y = self.y * rhs.y;
+		Self::new(x, y)
+	}
 }
 
 impl std::ops::Mul<f32> for Vec2 {
-    type Output = Self;
+	type Output = Self;
 
-    fn mul(self, rhs: f32) -> Self {
-        let x = self.x * rhs;
-        let y = self.y * rhs;
-        Self::new(x, y)
-    }
+	fn mul(self, rhs: f32) -> Self {
+		let x = self.x * rhs;
+		let y = self.y * rhs;
+		Self::new(x, y)
+	}
 }
 
 impl std::ops::Div<Self> for Vec2 {
-    type Output = Self;
+	type Output = Self;
 
-    fn div(self, rhs: Self) -> Self {
-        let x = self.x / rhs.x;
-        let y = self.y / rhs.y;
-        Self::new(x, y)
-    }
+	fn div(self, rhs: Self) -> Self {
+		let x = self.x / rhs.x;
+		let y = self.y / rhs.y;
+		Self::new(x, y)
+	}
 }
 
 impl std::ops::Div<f32> for Vec2 {
-    type Output = Self;
+	type Output = Self;
 
-    fn div(self, rhs: f32) -> Self {
-        let x = self.x / rhs;
-        let y = self.y / rhs;
-        Self::new(x, y)
-    }
+	fn div(self, rhs: f32) -> Self {
+		let x = self.x / rhs;
+		let y = self.y / rhs;
+		Self::new(x, y)
+	}
 }
 
 impl std::ops::Sub for Vec2 {
-    type Output = Self;
+	type Output = Self;
 
-    fn sub(self, rhs: Self) -> Self {
-        let x = self.x - rhs.x;
-        let y = self.y - rhs.y;
-        Self::new(x, y)
-    }
+	fn sub(self, rhs: Self) -> Self {
+		let x = self.x - rhs.x;
+		let y = self.y - rhs.y;
+		Self::new(x, y)
+	}
 }
 
 impl std::ops::Add for Vec2 {
-    type Output = Self;
+	type Output = Self;
 
-    fn add(self, rhs: Self) -> Self {
-        let x = self.x + rhs.x;
-        let y = self.y + rhs.y;
-        Self::new(x, y)
-    }
+	fn add(self, rhs: Self) -> Self {
+		let x = self.x + rhs.x;
+		let y = self.y + rhs.y;
+		Self::new(x, y)
+	}
 }
